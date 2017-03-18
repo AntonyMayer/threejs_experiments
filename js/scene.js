2ef600a9d3d030cc184007f8cc0c620be52dca7c
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-18 02:21:19 -0500

    animated rotation added

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,5 @@
 // Set up the scene, camera, and renderer as global variables.
-let scene,
+var scene,
     camera,
     renderer;
 
@@ -16,7 +16,7 @@ function init() {
         loader,
         axis,
         elm = {};
-        
+
     // Create a renderer and add it to the DOM.
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(WIDTH, HEIGHT);
@@ -35,23 +35,17 @@ function init() {
     // light.position.set(-100, 200, 100);
     let spotLight = new THREE.SpotLight(0xFFFFFF);
     spotLight.castShadow = true;
-    spotLight.position.set(15,30,50);
+    spotLight.position.set(15, 30, 50);
 
-
-    let planeGeometry = new THREE.PlaneGeometry(30,30,30);
-    let planeMaterial = new THREE.MeshLambertMaterial({color: 0xDDDDDD});
+    let planeGeometry = new THREE.PlaneGeometry(30, 30, 30);
+    let planeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD });
     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -.5*Math.PI;
+    plane.rotation.x = -.5 * Math.PI;
     plane.receiveShadow = true;
 
     // Add object
     loader = new THREE.JSONLoader();
-    loader.load('test.json', function(geometry) {
-        var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
-        test = new THREE.Mesh(geometry, material);
-        test.castShadow = true;
-        scene.add(test);
-    });
+    loader.load('test.json', loadCallback);
 
     // Add OrbitControls so that we can pan around with the mouse.
     controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -60,19 +54,32 @@ function init() {
     axis = new THREE.AxisHelper(10);
 
     //add grid
-    let grid = new THREE.GridHelper(50,5);
+    let grid = new THREE.GridHelper(50, 5);
     let gridColor = new THREE.Color("rgb(255,0,0)");
-    grid.setColors(gridColor, 0x000000); 
+    grid.setColors(gridColor, 0x000000);
 
     addToScene(scene, [camera, light, axis, plane, spotLight]);
 }
 
 // Renders the scene and updates the render as needed.
 function animate() {
-    scene.rotation.x += .1;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     controls.update();
+}
+
+function loadCallback(geometry) {
+    var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
+    test = new THREE.Mesh(geometry, material);
+    test.castShadow = true;
+    scene.add(test);
+    animateRotate();
+}
+
+function animateRotate() {
+    test.rotation.y += .01;
+    requestAnimationFrame(animateRotate);
+    renderer.render(scene, camera);
 }
 
 function addToScene(scene, objects) {
