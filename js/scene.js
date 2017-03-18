fb3adb25d43bf9937e4d7fb8777396d6f5ba3118
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-18 01:51:05 -0500

    promise load json works

--- js/scene.js
+++ js/scene.js
@@ -10,11 +10,12 @@ animate();
 function init() {
     // Create the scene and set the scene size.
     scene = new THREE.Scene();
-    var WIDTH = window.innerWidth,
+    let WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight,
         light,
         loader,
-        axis;
+        axis,
+        elm = {};
 
     // Create a renderer and add it to the DOM.
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -30,25 +31,29 @@ function init() {
     camera.lookAt(scene.position);
 
     // Create a light, set its position, and add it to the scene.
-    // light = new THREE.PointLight(0xffffff);
-    // light.position.set(-100, 200, 100);
     let spotLight = new THREE.SpotLight(0xFFFFFF);
     spotLight.castShadow = true;
-    spotLight.position.set(15,30,50);
+    spotLight.position.set(15, 30, 50);
 
-
-    let planeGeometry = new THREE.PlaneGeometry(30,30,30);
-    let planeMaterial = new THREE.MeshLambertMaterial({color: 0xDDDDDD});
+    let planeGeometry = new THREE.PlaneGeometry(30, 30, 30);
+    let planeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD });
     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -.5*Math.PI;
+    plane.rotation.x = -.5 * Math.PI;
     plane.receiveShadow = true;
 
     // Add object
     loader = new THREE.JSONLoader();
-    loader.load('test.json', function(geometry) {
-        // var material = new THREE.MeshBasicMaterial({ color: 0xFF0000, wireframe: true });
-        var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
-        test = new THREE.Mesh(geometry, material);
+    let promiseLoadObject = new Promise((resolve, reject) => {
+        loader.load('test.json', function(geometry) {
+            elm.material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
+            elm.geometry = geometry;
+            resolve();
+        });
+    });
+
+    promiseLoadObject.then(() => {
+        console.log(elm);
+        test = new THREE.Mesh(elm.geo, elm.material);
         test.castShadow = true;
         scene.add(test);
     });
@@ -60,15 +65,16 @@ function init() {
     axis = new THREE.AxisHelper(10);
 
     //add grid
-    let grid = new THREE.GridHelper(50,5);
+    let grid = new THREE.GridHelper(50, 5);
     let gridColor = new THREE.Color("rgb(255,0,0)");
-    grid.setColors(gridColor, 0x000000); 
+    grid.setColors(gridColor, 0x000000);
 
     addToScene(scene, [camera, light, axis, plane, spotLight]);
 }
 
 // Renders the scene and updates the render as needed.
 function animate() {
+    scene.rotation.x += .1;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     controls.update();
