d8fc67eff2f7c8641c010e46808941363d91639e
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-19 18:03:54 -0500

    cleaning

--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,3 @@
-// Set up the scene, camera, and renderer as global variables.
 var scene,
     camera,
     renderer,
@@ -16,18 +15,21 @@ init();
 animate();
 createGUI(guiControls);
 
-// Sets up the scene.
 function init() {
-    // Create the scene and set the scene size.
-    scene = new THREE.Scene();
     var WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight,
         light,
         loader,
-        axis,
         elm = {};
 
-    // Create a renderer and add it to the DOM.
+    /**
+     * SCENE
+     */
+     scene = new THREE.Scene();
+
+    /**
+     * RENDERER
+     */
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(WIDTH, HEIGHT);
     renderer.shadowMapEnabled = true;
@@ -35,21 +37,27 @@ function init() {
     renderer.domElement.id = "experiment";
     document.body.appendChild(renderer.domElement);
 
-    // Create a camera, zoom it out from the model a bit, and add it to the scene.
+    /**
+     * CAMERA
+     */
     camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 1000);
-    camera.position.set(15, 15, 15);
+    camera.position.set(15, 35, 15);
     camera.lookAt(scene.position);
 
-    // Create a light, set its position, and add it to the scene.
-    // light = new THREE.PointLight(0xffffff);
-    // light.position.set(-100, 200, 100);
+    /**
+     * LIGHT
+     */
     let spotLight = new THREE.SpotLight(0xFFFFFF);
     spotLight.castShadow = true;
     spotLight.position.set(15, 30, 50);
 
-    let planeGeometry = new THREE.PlaneGeometry(30, 30, 30);
-    let planeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD });
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    /**
+     * PLANE
+     */
+    let planeGeometry = new THREE.PlaneGeometry(30, 30, 30),
+        planeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD }),
+        plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    
     plane.rotation.x = -.5 * Math.PI;
     plane.receiveShadow = true;
 
@@ -60,12 +68,14 @@ function init() {
     // Add OrbitControls so that we can pan around with the mouse.
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    //add grid
-    let grid = new THREE.GridHelper(50, 5);
-    let gridColor = new THREE.Color("rgb(255,0,0)");
-    grid.setColors(gridColor, 0x000000);
+    addToScene([camera, light, plane, spotLight]);
+}
 
-    addToScene([camera, light, axis, plane, spotLight]);
+
+class Figure {
+  constructor() {
+
+  }
 }
 
 // Renders the scene and update orbit mouse controls
